9b34e540589c90ad1f1c38f0373b0bb96e4099d7
Florent Michon <florent.michon@gmail.com>
2016-12-01 21:48:47 +0100

    Positionne le theme RTD par défaut.

--- source/conf.py
+++ source/conf.py
@@ -10,7 +10,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
